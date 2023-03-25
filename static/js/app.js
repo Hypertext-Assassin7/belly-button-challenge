2257848c2398b2a979d20e0942fe933e6307dc63
Rayjong <rayjong7@gmail.com>
2023-03-25 14:34:44 +1100

    challenge completed

--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,33 @@
 // Get the endpoint
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+// Set variable for dataset
+var bellydata;
 
-var csvData;
 // Initialize the function 
 function init() {
 
-  // Use D3 to select the dropdown menu
-  
-
-  // Use D3 to get the options for drop down Menu
+// Use D3 to get the options for drop down Menu
   d3.json(url).then((data) => {
-    csvData = data;  
-    console.log(data)
-    // fill drop down
-    // draw chart 
-    fillDrowpDown(csvData);
+    bellydata = data; 
+    console.log(data) 
+
+// fill drop down
+fillDrowpDown(bellydata);
 
   });
 };
 
 function fillDrowpDown(data){
   let dropdownMenu = d3.select("#selDataset");
-    // Set a variable for the sample names
-  let names = data.names;
+    
+// Set a variable for the sample names
+  let samples = data.names;
 
   // Add  samples to dropdown menu
-  names.forEach((id) => {
+  samples.forEach((id) => {
 
-      // Log the value of id 
+  // Log the value of id 
       console.log(id);
 
       dropdownMenu.append("option")
@@ -37,7 +36,7 @@ function fillDrowpDown(data){
   });
 
   // Set the first sample from the list
-  let sample_one = names[0];
+  let sample_one = samples[0];
   buildDashboard(sample_one, data);
 }
 // Function that populates metadata info
@@ -52,14 +51,14 @@ function buildMetadata(sample, data) {
   console.log(value)
 
   // Get the first index from the array
-  let valueData = value[0];
+  let firstvalue = value[0];
 
   // Clear out metadata
   d3.select("#sample-metadata").html("");
 
   // Use Object.entries to add each key/value pair to the panel
 
-  Object.entries(valueData).forEach(([key,value]) => {
+  Object.entries(firstvalue).forEach(([key,value]) => {
 
       // Log the individual key/value pairs as they are being appended to the metadata panel
       console.log(key,value);
@@ -73,31 +72,31 @@ function buildMetadata(sample, data) {
 function buildBarChart(sample, data) {
 
 
-  let sampleInfo = data.samples;
+  let samplevalue = data.samples;
 
   // Filter based on the value of the sample
-  let value = sampleInfo.filter(result => result.id == sample);
+  let value = samplevalue.filter(result => result.id == sample);
 
   // Get the first index from the array
-  let valueData = value[0];
+  let firstsample = value[0];
 
   // Get the otu_ids, lables, and sample values
-  let otu_ids = valueData.otu_ids;
-  let otu_labels = valueData.otu_labels;
-  let sample_values = valueData.sample_values;
+  let otu_ids = firstsample.otu_ids;
+  let otu_labels = firstsample.otu_labels;
+  let sample_values = firstsample.sample_values;
 
   // Log the data to the console
   console.log(otu_ids,otu_labels,sample_values);
 
   // Set top ten items to display in descending order
-  let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
-  let xticks = sample_values.slice(0,10).reverse();
+  let yaxis = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+  let xaxis = sample_values.slice(0,10).reverse();
   let labels = otu_labels.slice(0,10).reverse();
   
   // Set up the trace for the bar chart
   let trace = {
-      x: xticks,
-      y: yticks,
+      x: xaxis,
+      y: yaxis,
       text: labels,
       type: "bar",
       orientation: "h"
@@ -136,7 +135,7 @@ function buildBubbleChart(sample, data) {
        marker: {
            size: sample_values,
            color: otu_ids,
-           colorscale: "Earth"
+           colorscale: "Viridis"
        }
    };
 
@@ -168,15 +167,15 @@ function buildGaugeChart(sample, data) {
     gauge: {
     axis: { range: [null, 9] },
     steps: [
-    { range: [0, 2], color: "white",text:"0-1" },
-    { range: [2, 4], color: "lightgray",text:"1-2" },
-    { range: [4, 6], color: "gray",text:"2-3" },
-    { range: [6, 8], color: "lightblue",text:"3-4" },
-    { range: [8, 10], color: "babyblue",text:"4-5" },
-    { range: [5, 6], color: "blue",text:"5-6" },
-    { range: [6, 7], color: "lightred",text:"6-7" },
-    { range: [7, 8], color: "red",text:"7-8" },
-    { range: [8, 9], color: "black",text:"8-9" }
+    { range: [0, 1], color: "rgba(255, 0, 0, 1)",text:"0-1" },
+    { range: [1, 2], color: "rgba(173, 216, 230, 1)",text:"1-2" },
+    { range: [2, 3], color: "rgba(0, 0, 255, 1)",text:"2-3" },
+    { range: [3, 4], color: "rgba(0, 0, 0, 1)",text:"3-4" },
+    { range: [4, 5], color: "rgba(255, 255, 255, 1)",text:"4-5" },
+    { range: [5, 6], color: "rgba(255, 255, 0, 1)",text:"5-6" },
+    { range: [6, 7], color: "rgba(255, 165, 0, 1)",text:"6-7" },
+    { range: [7, 8], color: "rgba(128, 0, 128, 1)",text:"7-8" },
+    { range: [8, 9], color: "rgba(255, 192, 203, 1)",text:"8-9" }
     ]
     }
   }];
@@ -192,10 +191,10 @@ function buildDashboard(value) {
   console.log(value); 
 
   // Call all functions 
-  buildMetadata(value, csvData);
-  buildBarChart(value, csvData);
-  buildBubbleChart(value, csvData);
-  buildGaugeChart(value, csvData);
+  buildMetadata(value, bellydata);
+  buildBarChart(value, bellydata);
+  buildBubbleChart(value, bellydata);
+  buildGaugeChart(value, bellydata);
 };
 
 // Call the initialize function
